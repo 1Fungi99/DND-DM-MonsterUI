5dc2c632c5b58efb43d571685b4f7f54deeba1c3
Justin Cheng <ijustin17@gmail.com>
2020-03-08 15:30:13 -0500

    added more functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends Component {
     monsterQuery: "",
 
     //Monster Name
-    monsterName: "",
+    monsterName: "Search a Monster!",
 
     //Ability Score
     monsterSTR: "",
@@ -18,7 +18,15 @@ export default class Home extends Component {
     monsterCHA: "",
 
     //Monster AC
-    monsterAC: ""
+    monsterAC: "",
+
+    //Monster Speed
+    monsterSpeed: [],
+
+    monsterHP: "",
+    monsterHitDice: "",
+
+    monsterProf: []
   };
 
   handleChange = event => {
@@ -30,10 +38,21 @@ export default class Home extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    this.setState({ monsterQuery: this.state.monsterQueryChange });
+    this.setState({
+      monsterQuery: this.state.monsterQueryChange
+        .split(" ")
+        .join("-")
+        .toLowerCase()
+    });
     // console.log("Monster Query: " + this.state.monsterQuery);
     axios
-      .get(this.baseURL + this.state.monsterQueryChange.toLowerCase())
+      .get(
+        this.baseURL +
+          this.state.monsterQueryChange
+            .split(" ")
+            .join("-")
+            .toLowerCase()
+      )
       .then(res => {
         const data = res.data;
         console.log(data);
@@ -53,10 +72,45 @@ export default class Home extends Component {
       monsterCON: data.constitution,
       monsterINT: data.intelligence,
       monsterWIS: data.wisdom,
-      monsterCHA: data.charisma
+      monsterCHA: data.charisma,
+      monsterSpeed: data.speed,
+      monsterHP: data.hit_points,
+      monsterHitDice: data.hit_dice,
+      monsterProf: data.proficiencies
     });
     console.log(this.state);
+    this.speedData();
+    this.profData();
   };
+
+  speedData = () => {
+    var speedArray = [];
+    for (var key in this.state.monsterSpeed) {
+      if (this.state.monsterSpeed.hasOwnProperty(key)) {
+        speedArray.push(key + ": " + this.state.monsterSpeed[key]);
+      }
+    }
+    speedArray = speedArray.join(" , ");
+    return <h5>Speed: {speedArray}</h5>;
+  };
+
+  profData = () => {
+    var profArray = [];
+    for (var i = 0; i < this.state.monsterProf.length; i++) {
+      for (var key in this.state.monsterProf[i]) {
+        if (this.state.monsterProf[i].hasOwnProperty(key)) {
+          profArray.push(key + ": " + this.state.monsterProf[i][key]);
+        }
+      }
+    }
+    // profArray.filter();
+    console.log(profArray);
+  };
+
+  // profFilter = () =>{
+  //   return( if (!profArray.includes("url:"))
+  //   )
+  // }
 
   render() {
     return (
@@ -88,6 +142,31 @@ export default class Home extends Component {
 
         <div className="container monsterContainer">
           <h3>Searched Monster:</h3>
+          <h4 className="align-left">{this.state.monsterName}</h4>
+          <div className="row">
+            <div className="col-sm-5 align-left">
+              <h5>Hit Points: {this.state.monsterHP}</h5>
+              <h5>Hit Die: {this.state.monsterHitDice}</h5>
+              <h5>Armor Class: {this.state.monsterAC}</h5>
+              <this.speedData />
+              <div className="row">
+                <h5 className="row-spacing">STR</h5>
+                <h5 className="row-spacing">DEX</h5>
+                <h5 className="row-spacing">CON</h5>
+                <h5 className="row-spacing">INT</h5>
+                <h5 className="row-spacing">WIS</h5>
+                <h5 className="row-spacing">CHA</h5>
+              </div>
+              <div className="row">
+                <h5 className="row-spacing">{this.state.monsterSTR}</h5>
+                <h5 className="row-spacing">{this.state.monsterDEX}</h5>
+                <h5 className="row-spacing">{this.state.monsterCON}</h5>
+                <h5 className="row-spacing">{this.state.monsterINT}</h5>
+                <h5 className="row-spacing">{this.state.monsterWIS}</h5>
+                <h5 className="row-spacing">{this.state.monsterCHA}</h5>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
